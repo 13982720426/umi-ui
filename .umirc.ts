adbb088d5d97d6c2bad89cfbd823027fc9c2189c
13982720426 <1679575151@qq.com>
2021-05-06 22:06:00 +0800

    第一次使用umi-ui，简单配置

---
+++ .umirc.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'umi';
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    {
+        name: '注册页',
+        path: '/userregister',
+        component: './UserRegister',
+    },
+    {
+      path: '/',
+      component: '@/layouts/LayoutSide/index',
+      routes: [
+        {
+          name: '空白页面',
+          path: '/user',
+          component: './User',
+        },
+        {
+          path: '/',
+          component: '@/pages/index',
+        },
+      ],
+    },
+  ],
+  fastRefresh: {},
+    dva: {
+        immer: true,
+        hmr: false,
+    },
+    locale: {
+        default: 'zh-CN',
+        antd: false,
+        title: false,
+        baseNavigator: true,
+        baseSeparator: '-',
+    },
+});
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.less';
+import IndexTable from './IndexTable';
+import ButtonBasic from './ButtonBasic';
+export default function IndexPage() {
+  return (
+    <div>
+      <IndexTable />
+      <ButtonBasic />
+    </div>
+  );
+}
